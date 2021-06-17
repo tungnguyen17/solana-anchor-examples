79348c8605802d8180d8515e5951a1b7a95b7d87
Tung Nguyen <2715595+tungnguyen17@users.noreply.github.com>
2021-06-17 11:59:28 +0700

    Try minimize import

--- indirect_transfer_example/programs/transfer_spl_token/src/lib.rs
+++ indirect_transfer_example/programs/transfer_spl_token/src/lib.rs
@@ -1,6 +1,21 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{ AccountDeserialize, AccountSerialize, Accounts, AccountsExit, AnchorDeserialize, AnchorSerialize, program, Context,
+  prelude::{ AccountInfo, borsh, msg, ProgramError, ProgramResult, Pubkey },
+};
 
 #[program]
 mod indirect_transfer {
   use super::*;
+
+  pub fn transfer_sol(
+    ctx: Context<TransferSolContext>,
+    amount: u64,
+  ) -> ProgramResult {
+    msg!("Instruction: Transfer SOL");
+    Ok(())
+  }
+}
+
+#[derive(Accounts)]
+pub struct TransferSolContext<'info> {
+  pub recipient: AccountInfo<'info>,
 }
