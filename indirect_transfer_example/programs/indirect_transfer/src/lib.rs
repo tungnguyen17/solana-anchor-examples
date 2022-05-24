cf7287605050333b0dd3ba08a274accdae71e9d4
Tung Nguyen <2715595+tungnguyen17@users.noreply.github.com>
2022-05-24 16:11:23 +0700

    bump indirect_transfer_example to anchor 0.24.2

--- indirect_transfer_example/programs/indirect_transfer/src/lib.rs
+++ indirect_transfer_example/programs/indirect_transfer/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 mod indirect_transfer {
@@ -8,23 +9,26 @@ mod indirect_transfer {
   pub fn transfer_sol(
     ctx: Context<TransferSolContext>,
     amount: u64,
-  ) -> ProgramResult {
+  ) -> Result<()> {
     msg!("Instruction: Transfer SOL");
+
     let payer = &ctx.accounts.payer;
     let sender = &ctx.accounts.sender;
     let recipient = &ctx.accounts.recipient;
-    let system_program = &ctx.accounts.system_program;
     let instruction = &solana_program::system_instruction::transfer(sender.key, recipient.key, amount);
     msg!("DEBUG: Transfer Instruction {:?}", instruction);
-    solana_program::program::invoke(&instruction, &[payer.clone(), sender.clone(), recipient.clone()]);
+    solana_program::program::invoke(&instruction, &[payer.clone(), sender.clone(), recipient.clone()])
+      .expect("CPI failed");
+
     Ok(())
   }
 
   pub fn transfer_token(
     ctx: Context<TransferTokenContext>,
     amount: Vec<u8>,
-  ) -> ProgramResult {
+  ) -> Result<()> {
     msg!("Instruction: Transfer Token");
+
     let token_program = &ctx.accounts.token_program;
     let payer = &ctx.accounts.payer;
     let sender = &ctx.accounts.sender;
@@ -40,31 +44,50 @@ mod indirect_transfer {
       data: amount.clone(),
     };
     msg!("DEBUG: TransferToken Instruction {:?}", instruction);
-    solana_program::program::invoke(&instruction, &[payer.clone(), sender.clone(), sender_token.clone(), recipient_token.clone()]);
+    solana_program::program::invoke(&instruction, &[payer.clone(), sender.clone(), sender_token.clone(), recipient_token.clone()])
+      .expect("CPI failed");
+
     Ok(())
   }
 }
 
 #[derive(Accounts)]
 pub struct TransferSolContext<'info> {
+
+  /// CHECK: Transaction fee payer
   #[account(signer)]
   pub payer: AccountInfo<'info>,
+
+  /// CHECK: Source account to send lamports
   #[account(signer)]
   pub sender: AccountInfo<'info>,
+
+  /// CHECK: Target account to receive lamports
   #[account(mut)]
   pub recipient: AccountInfo<'info>,
-  pub system_program: AccountInfo<'info>,
+
+  pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct TransferTokenContext<'info> {
+
+  /// CHECK: Transaction fee payer
   #[account(signer)]
   pub payer: AccountInfo<'info>,
+
+  /// CHECK: Owner of source account
   #[account(signer)]
   pub sender: AccountInfo<'info>,
+
+  /// CHECK: Source account to send tokens
   #[account(mut)]
   pub sender_token: AccountInfo<'info>,
+
+  /// CHECK: Target account to receive tokens
   #[account(mut)]
   pub recipient_token: AccountInfo<'info>,
+
+  /// CHECK: Solana's native token program
   pub token_program: AccountInfo<'info>,
 }
